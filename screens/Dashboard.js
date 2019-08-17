b02f00741ab58b9384b7b9de66450a445d86bbf0
Keshav Ashiya <keshavashiya@outlook.com>
2019-08-17 15:25:39 +0530

    Graph ploted

---
+++ screens/Dashboard.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import {
+    StyleSheet,
+    Text,
+    View,
+    ScrollView
+} from 'react-native';
+
+import Chart from '../components/Chart';
+
+const Dashboard = () => {
+  return (
+    <ScrollView>
+      <View style={styles.item}>
+        <Text style={styles.text}>Error Rate by Day</Text>
+        <Chart type="line" />
+      </View>
+    </ScrollView>
+  );
+};
+
+
+const styles = StyleSheet.create({
+  item: {
+    padding: 0,
+    fontSize: 18,
+    height: 400,
+    borderWidth: 0.5,
+    borderColor: '#d6d7da',
+  },
+  text: {
+    fontSize: 24,
+    padding: 10,
+    fontWeight: "bold",
+  }
+})
+
+Dashboard.navigationOptions = {
+  title: 'Dashboard',
+  headerForceInset: { top: 'never', bottom: 'never' }
+};
+
+export default Dashboard;
